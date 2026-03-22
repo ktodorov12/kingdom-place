fff0d2221dac73ab6c3f1291eb139c22bfab5758
ktodorov12 <k_todorov12@abv.bg>
2026-03-22 14:01:17 +0200

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  // TODO: Change '/kingdom-place/' to your actual GitHub repository name if different
+  base: command === "build" ? "/kingdom-place/" : "/",
   plugins: [react(), tailwindcss()],
-});
+}));
