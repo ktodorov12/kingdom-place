38bca52bb8c6292e5a6da6d5ccf3d1c65f20473f
ktodorov12 <k_todorov12@abv.bg>
2026-03-22 14:27:10 +0200

    chore: change deployment to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => ({
-  // TODO: Change '/kingdom-place/' to your actual GitHub repository name if different
-  base: command === "build" ? "/kingdom-place/" : "/",
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-}));
+});
